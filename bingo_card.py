56b19cc8656e6dbcdc095ab5390c107fa59af9f8
Yuya-Omachi <omachiinoken@gmail.com>
2023-04-13 14:27:52 +0900

    ひとまず完成

--- bingo_card.py
+++ bingo_card.py
@@ -12,6 +12,7 @@ class BingoCell(ABC):
     def __str__(self) -> str:
         pass
 
+    # 数字をチェックし、合っているなら穴を開ける
     @abstractmethod
     def open(self, number: int) -> None:
         pass
@@ -66,3 +67,53 @@ class BingoCard:
         for line in formatted:
             print(*line)
 
+    def fill_cell(self, number: int) -> None:
+        # 各セルにopenメソッドを適用
+        fill = lambda e: e.open(number)
+        vfunc = np.vectorize(fill)
+        vfunc(self.cells)
+
+    def count_filled_in_line(self, line_indexes: list[tuple[int,int]]) -> int:
+        filled = 0
+        for index in line_indexes:
+            # indexに対応したマスが開いているなら1を足し合わせる
+            filled += 1 if self.cells[index[0],index[1]].is_opened else 0
+        return filled
+
+    def count_bingo_num(self) -> int:
+        bingo_line_num = 0
+        check_lines = [
+            [(0,0),(0,1),(0,2),(0,3),(0,4)],
+            [(1,0),(1,1),(1,2),(1,3),(1,4)],
+            [(2,0),(2,1),(2,2),(2,3),(2,4)],
+            [(3,0),(3,1),(3,2),(3,3),(3,4)],
+            [(0,0),(1,0),(2,0),(3,0),(4,0)],
+            [(0,1),(1,1),(2,1),(3,1),(4,1)],
+            [(0,2),(1,2),(2,2),(3,2),(4,2)],
+            [(0,3),(1,3),(2,3),(3,3),(4,3)],
+            [(0,0),(1,1),(2,2),(3,3),(4,4)],
+            [(0,4),(1,3),(2,2),(3,1),(4,0)]
+        ]
+        for line in check_lines:
+            # 開いているマスが5つの列をビンゴとしてカウント
+            bingo_line_num += 1 if self.count_filled_in_line(line) == 5 else 0
+        return bingo_line_num
+
+    def count_reach_num(self) -> int:
+        reach_line_num = 0
+        check_lines = [
+            [(0,0),(0,1),(0,2),(0,3),(0,4)],
+            [(1,0),(1,1),(1,2),(1,3),(1,4)],
+            [(2,0),(2,1),(2,2),(2,3),(2,4)],
+            [(3,0),(3,1),(3,2),(3,3),(3,4)],
+            [(0,0),(1,0),(2,0),(3,0),(4,0)],
+            [(0,1),(1,1),(2,1),(3,1),(4,1)],
+            [(0,2),(1,2),(2,2),(3,2),(4,2)],
+            [(0,3),(1,3),(2,3),(3,3),(4,3)],
+            [(0,0),(1,1),(2,2),(3,3),(4,4)],
+            [(0,4),(1,3),(2,2),(3,1),(4,0)]
+        ]
+        for line in check_lines:
+            # 開いているマスが5つの列をビンゴとしてカウント
+            reach_line_num += 1 if self.count_filled_in_line(line) == 4 else 0
+        return reach_line_num
--- main.py
+++ main.py
@@ -2,4 +2,15 @@ from bingo_card import *
 
 card = BingoCard()
 
+balls = list(range(1,76))
+random.shuffle(balls)
+
 card.print_cells()
+
+for i, ball in enumerate(balls):
+    card.fill_cell(ball)
+    print(f"REACH: {card.count_reach_num()}")
+    print(f"BINGO: {card.count_bingo_num()}")
+    print("--------------------")
+    print(f"ball[{i}]: {ball}")
+    card.print_cells()
