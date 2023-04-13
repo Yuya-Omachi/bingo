22b8f5530768ae4425c699a0dcb9fa81861d0de4
Yuya-Omachi <omachiinoken@gmail.com>
2023-04-13 14:34:33 +0900

    ビンゴの判定の修正

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ card = BingoCard()
 balls = list(range(1,76))
 random.shuffle(balls)
 
+print("your card")
 card.print_cells()
 
 for i, ball in enumerate(balls):
     card.fill_cell(ball)
-    print(f"REACH: {card.count_reach_num()}")
-    print(f"BINGO: {card.count_bingo_num()}")
-    print("--------------------")
     print(f"ball[{i}]: {ball}")
     card.print_cells()
+    print(f"\nREACH: {card.count_reach_num()}")
+    print(f"BINGO: {card.count_bingo_num()}")
+    print("-----------------------")
